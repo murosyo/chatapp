0964d411c1e1ddd0c1b900fce9ada73e839b18f5
murosyo <murozuka.shota@mikilab.doshisha.ac.jp>
2023-08-22 16:37:10 +0900

    test

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -20,7 +20,9 @@ const inputUserName = ref("")
 // 入室メッセージをクライアントに送信する
 const onEnter = () => {
   // ユーザー名が入力されているかチェック
-
+  if (inputUserName === ''){
+    console.log('ユーザ名を入力してください。')
+  }
   // 入室メッセージを送信
 
   // 全体で使用するnameに入力されたユーザー名を格納
--- chatapp/src/router/index.js
+++ chatapp/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
-import Login from "../components/Chat.vue"
-import Chat from "../components/Login.vue"
+import Login from "../components/Login.vue"
+import Chat from "../components/Chat.vue"
 
 const router = createRouter({
   history: createWebHistory(),
