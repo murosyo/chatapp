fe8c93cd34749939ec84c23193cb89492d4279bf
murosyo <murozuka.shota@mikilab.doshisha.ac.jp>
2023-09-11 15:07:44 +0900

    データベースの値確認

--- chatapp/socket_event/index.js
+++ chatapp/socket_event/index.js
@@ -10,37 +10,47 @@ const chatlog_db = new sqlite3.Database(path.join(process.cwd(), 'chatlog.db'));
 console.log("データベースに接続完了");
 
 export default (io, socket) => {
+  userinfo_db.each("select * from user_info;", (err, row) => {
+    console.log(row['name'], row['password'], row['room']);
+    // console.log(row['password']);
+    // callback({
+    //   name:row['name'],
+    //   password:row['password'],
+    //   room:row['room'],
+    //   data: row
+    // });
+  })
+
   // 入室メッセージをクライアントに送信する
   socket.on("enterEvent", (userName, password, room, callback) => {
     // console.log(userName, password, room)
-    userinfo_db.each("select count(*) from user_info where name = '"+ userName + "' AND password = '" + password + "';", (err, row) => {
+
+    userinfo_db.each("select count(*), name, password, room from user_info where name = '"+ userName + "' AND password = '" + password + "';", (err, row) => {
+    // userinfo_db.each("select * from user_info;", (err, row) => {  
       if (row['count(*)'] > 0) {
         callback({
-          status:"SIGN IN"
+          status:"SIGN IN",
+          name:row['name'],
+          password:row['password'],
+          room:row['room'],
+          data: row
         });
-        userinfo_db.each("select name, password from user_info;", (err, row) => {
-          console.log(row['name'])
-          callback({
-            status:"OK"
-          });
-        })
-        socket.broadcast.emit("enterEvent", userName + "さんが" + room + "に入室しました。")
+        // socket.broadcast.emit("enterEvent", userName + "さんが" + room + "に入室しました。")
       }
       else {
         callback({
-          status:"SIGN UP"
+          status:"SIGN UP",
+          name:row['name'],
+          password:row['password'],
+          room:row['room'],
+          data: row
         });
-        userinfo_db.each("select name, password from user_info;", (err, row) => {
-          console.log(row['name'])
-          callback({
-            status:"OK"
-          });
-        })
         userinfo_db.run("INSERT INTO user_info(name, password, room) VALUES('" + userName + "', '" + password + "', '" + room + "');");
-        socket.broadcast.emit("enterEvent", userName + "さんが" + room + "に入室しました。")
+        // socket.broadcast.emit("enterEvent", userName + "さんが" + room + "に入室しました。")
       }
-    }
-    )
+    })
+
+    socket.broadcast.emit("enterEvent", userName + "さんが" + room + "に入室しました。")
     // userinfo_db.close();
     // socket.broadcast.emit("enterEvent", data)
   })
